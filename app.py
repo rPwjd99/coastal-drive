84664ac03311b3a390495903c413d3b6bbd1e311
rPwjd99 <snow128@hanmail.net>
2025-05-09 10:38:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def route():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render가 자동 지정한 PORT 사용
+    app.run(host="0.0.0.0", port=port, debug=True)
+
